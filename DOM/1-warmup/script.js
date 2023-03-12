7389b9261bde5fad157ee0cbea94f265d8dd0e84
Jakub Meyza <kubameyza@gmail.com>
2023-03-12 13:09:43 +0100

    Add index.html, script.js and style.css files

---
+++ DOM/1-warmup/script.js
@@ -0,0 +1,8 @@
+console.log("DOM API");
+
+const header = document.getElementById("header");
+console.log(header);
+console.log(header.in);
+
+header.classList.add("header");
+header.innerText = "I am heading";
