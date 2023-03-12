040f2b64dedbb6f234db2ee8f27ae5db6f9ae7d0
Jakub Meyza <kubameyza@gmail.com>
2023-03-12 19:12:04 +0100

    Complete warm-up

--- DOM/1-warmup/script.js
+++ DOM/1-warmup/script.js
@@ -2,7 +2,42 @@ console.log("DOM API");
 
 const header = document.getElementById("header");
 console.log(header);
-console.log(header.in);
 
 header.classList.add("header");
 header.innerText = "I am heading";
+
+const listItems = ["a", "b", "c", "d"];
+
+let list = document.createElement("ol");
+
+listItems.forEach((item) => {
+  let element = document.createElement("li");
+  element.innerText = item;
+  list.append(element);
+});
+
+document.body.append(list);
+
+const items = document.querySelectorAll("li");
+
+items.forEach((item) => {
+  item.classList.add("listItem");
+});
+
+header.classList.remove("header");
+
+header.remove();
+
+function toggleBackgroundColor() {
+  if (document.body.style.backgroundColor === "rgb(18, 49, 35)") {
+    document.body.style.backgroundColor = "";
+  } else {
+    document.body.style.backgroundColor = "rgb(18, 49, 35)";
+  }
+}
+
+let button = document.createElement("button");
+button.append("Click Me");
+document.body.append(button);
+
+button.addEventListener("click", toggleBackgroundColor);
